ff649fdc3b3e723d64c34ca3ba6f9739c2d4c968
tuxPM <aissam@yahoo.com>
2022-06-28 03:56:49 +0200

    add various config retrieve

--- src/db.php
+++ src/db.php
@@ -1,8 +1,10 @@
 <?php
 var_dump($_ENV);
 var_dump(getenv());
-if (!getenv("CONFIG_PATH")) 
-    die("no config set, please set CONFIG_PATH environment variable");
-$config=parse_ini_file(getenv("CONFIG_PATH"));
+define('CONFIG_ENV', 'APP_CONFIG_PATH');
+if (!getenv(CONFIG_ENV) && !$_SEVER[CONFIG_ENV] && !$_ENV[CONFIG_ENV]) 
+    die("no config set, please set ".CONFIG_ENV." environment variable");
+$config_path = isset($_SERVER[CONFIG_ENV]) ? $_SERVER['APP_CONFIG_PATH'] : (getenv(CONFIG_ENV)?getenv(CONFIG_ENV):$_ENV[CONFIG_ENV]);
+$config=parse_ini_file($config_path);
 
-mysqli_connect($config["host"],$config["user"],$config["password"]) || die("can't connect:".mysqli_connect_error());
+mysqli_connect($config["host"],$config["user"],$config["password"]) || die("can't connect:".vvmysqli_connect_error());
