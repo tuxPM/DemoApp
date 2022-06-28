f559cfd32733ede6852e87ff6eec2bae08a2564f
tuxPM <aissam@yahoo.com>
2022-06-28 05:47:16 +0200

    fix

--- src/db.php
+++ src/db.php
@@ -2,12 +2,11 @@
 var_dump($_ENV);
 var_dump(getenv());
 define('CONFIG_ENV', 'APP_CONFIG_PATH');
-if (!getenv(CONFIG_ENV) && !$_SEVER[CONFIG_ENV] && !$_ENV[CONFIG_ENV]) 
-    die("no config set, please set ".CONFIG_ENV." environment variable");
-$config_path = isset($_SERVER[CONFIG_ENV]) ? $_SERVER['APP_CONFIG_PATH'] : (getenv(CONFIG_ENV)?getenv(CONFIG_ENV):$_ENV[CONFIG_ENV]);
+if (!getenv(CONFIG_ENV) && !isset($_SERVER[CONFIG_ENV]) && !isset($_ENV[CONFIG_ENV])) 
+    die('no config set, please set '.CONFIG_ENV.' environment variable');
+$config_path = isset($_SERVER[CONFIG_ENV]) ? $_SERVER[CONFIG_ENV] : (getenv(CONFIG_ENV)?getenv(CONFIG_ENV):$_ENV[CONFIG_ENV]);
 $config=parse_ini_file($config_path);
 
-mysqli_connect() || die("can't connect:".vvmysqli_connect_error());
 $db=new mysqli($config["host"],$config["user"],$config["password"], $config['db']);
 if ($db->connect_errno)
     die("can't connect:{$db->connect_error}");
